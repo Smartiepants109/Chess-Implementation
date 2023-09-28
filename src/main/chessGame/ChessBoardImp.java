9a9e63226607612b3c9b23adfe05952ee9566a9c
Smartiepants109 <43049387+Smartiepants109@users.noreply.github.com>
2023-09-28 15:15:26 -0600

    Implemented Threefold Repetition

--- src/main/chessGame/ChessBoardImp.java
+++ src/main/chessGame/ChessBoardImp.java
@@ -1,14 +1,27 @@
 package chessGame;
 
+import chess.ChessBoard;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
 public class ChessBoardImp implements chess.ChessBoard {
+    private static final int BOARD_SIZE = 8;
+
     public ChessBoardImp() {
         resetBoard();
     }
 
     private ChessPiece[][] board;
+
+    public ChessBoardImp(ChessBoard oldBoard) {
+        board = new ChessPiece[BOARD_SIZE][BOARD_SIZE];
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                ChessPositionImp pos = new ChessPositionImp(i, j);
+                board[i][j] = oldBoard.getPiece(pos);
+            }
+        }
+    }
 
     @Override
     public void addPiece(ChessPosition position, ChessPiece piece) {
--- src/main/chessGame/ChessGameImp.java
+++ src/main/chessGame/ChessGameImp.java
@@ -1,31 +1,61 @@
 package chessGame;
 
-import chess.ChessBoard;
-import chess.ChessMove;
-import chess.ChessPosition;
-import chess.InvalidMoveException;
+import chess.*;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
-public class ChessGameImp implements chess.ChessGame{
+public class ChessGameImp implements chess.ChessGame {
+    TeamColor currentTurn;
+    ChessBoard board;
+    Map<ChessBoard, Integer> pastMoves;
+
+    public ChessGameImp() {
+        currentTurn = TeamColor.WHITE;
+        board = new ChessBoardImp();
+        pastMoves = new HashMap<ChessBoard, Integer>();
+    }
+
     @Override
     public TeamColor getTeamTurn() {
-        return null;
+        return currentTurn;
     }
 
     @Override
     public void setTeamTurn(TeamColor team) {
+        currentTurn = team;
+    }
 
+    /**
+     * ONLY USE FOR 2 TEAM GAMES. If 3 or 4 player games are ever implemented, delete this function
+     * and replace any uses with setTeamTurn.
+     */
+    public void toggleTeamTurn() {
+        if (currentTurn == TeamColor.WHITE) {
+            currentTurn = TeamColor.BLACK;
+        } else {
+            currentTurn = TeamColor.WHITE;
+        }
     }
 
     @Override
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
+        ChessPiece pieceType = getBoard().getPiece(startPosition);
+        return validMoveHelper(startPosition, pieceType);
+    }
+
+    private Collection<ChessMove> validMoveHelper(ChessPosition startPosition, ChessPiece pieceType) {
         return null;
     }
 
     @Override
     public void makeMove(ChessMove move) throws InvalidMoveException {
-
+        int count = 0;
+        if (pastMoves.containsKey(board)) {
+            count = pastMoves.get(board);
+            pastMoves.replace(board, count, count + 1);
+        }
     }
 
     @Override
@@ -40,16 +70,21 @@ public class ChessGameImp implements chess.ChessGame{
 
     @Override
     public boolean isInStalemate(TeamColor teamColor) {
+        for (Map.Entry<ChessBoard, Integer> entry : pastMoves.entrySet()) {
+            if (entry.getValue() >= 3) {
+                return true;
+            }
+        } //Threefold repetition implementation.
         return false;
     }
 
     @Override
     public void setBoard(ChessBoard board) {
-
+        this.board = board;
     }
 
     @Override
     public ChessBoard getBoard() {
-        return null;
+        return board;
     }
 }
